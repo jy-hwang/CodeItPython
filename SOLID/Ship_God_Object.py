b00955e2e9ba3070c9a69f80d27274c07f839af9
jy-hwang <stratojy@gmail.com>
2020-11-09 19:24:01 +0900

    codeitpython_1109

---
+++ SOLID/Ship_God_Object.py
@@ -0,0 +1,10 @@
+class Ship:
+    def __init__(self):
+        """연료량, 시간당 연료 소비량, 물자량, 선원 수를 인스턴스 변수로 갖는다"""
+
+
+    def  report_fuel(self):
+        """연료량 보고 메소드"""
+
+    def load_fuel(self,amount):
+        """연료 충전 메소드"""
